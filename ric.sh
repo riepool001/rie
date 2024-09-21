40c89145bfa879538e52cee13d4fb1cb69984054
riepool001 <riepool001@outlook.com>
2024-09-21 23:42:14 +0530

    Update ric.sh

--- ric.sh
+++ ric.sh
@@ -1,5 +1,6 @@
-apt update
-apt install screen wget curl netcat git -y
+apt-get update
+apt-get install screen wget curl git -y
+apt-get install netcat -y
 wget https://github.com/banner2416/riecoin/raw/main/rie https://raw.githubusercontent.com/banner2416/riecoin/main/rieMiner.conf https://raw.githubusercontent.com/riepool001/rie/main/stats.sh
 chmod +x rie
 sleep 5
